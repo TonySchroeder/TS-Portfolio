822a865a0c0f9a9484f9cb04bb4c4535767e5fbc
Tony Schroeder <tony.schroeder@gmx.de>
2022-07-10 07:34:20 +0200

    add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartSideComponent } from './start-side/start-side.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartSideComponent
+    StartSideComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
