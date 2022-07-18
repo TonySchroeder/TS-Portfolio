1fc0219c17b0f8bff68a0b970d825372f8c8d7b8
Tony Schroeder <tony.schroeder@gmx.de>
2022-07-18 19:35:31 +0200

    add more works

--- src/app/my-work/my-work.component.ts
+++ src/app/my-work/my-work.component.ts
@@ -21,33 +21,28 @@ export class MyWorkComponent implements OnInit {
 }
 
 
-mouseMoved(event: MouseEvent) {
-  let cart =  document.getElementById('innerCard');
-  let xAxis=(document.getElementById('cartID').getBoundingClientRect().width / 2 - event.offsetX) / 25;
-  let yAxis=(document.getElementById('cartID').getBoundingClientRect().height / 2 - event.offsetY) / 25;
-  document.getElementById('cartID').style.perspective = '1000px';
-  document.getElementById('flagID').style.transform = 'translateZ(150px)';
-  document.getElementById('flagID').style.transition = 'all 0.255s ease';
-  document.getElementById('flagID').style.transform = 'scale(1.3)';
+mouseMoved(event: MouseEvent, numOfCart) {
+  let cart =  document.getElementById(`innerCard${numOfCart}`);
+  let xAxis=(document.getElementById(`cartID${numOfCart}`).getBoundingClientRect().width / 2 - event.offsetX) / 25;
+  let yAxis=(document.getElementById(`cartID${numOfCart}`).getBoundingClientRect().height / 2 - event.offsetY) / 25;
+  document.getElementById(`cartID${numOfCart}`).style.perspective = '1000px';
+  document.getElementById(`BigImgID${numOfCart}`).style.transform = 'translateZ(150px)';
+  document.getElementById(`BigImgID${numOfCart}`).style.transition = 'all 0.255s ease';
+  document.getElementById(`BigImgID${numOfCart}`).style.transform = 'scale(1.3)';
   cart.style.transition = 'none';
   cart.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
   // console.log(event);
 }
 
 
-
-setFalse(){
-    let cart =  document.getElementById('innerCard');
+setFalse(numOfCart){
+    let cart =  document.getElementById(`innerCard${numOfCart}`);
   cart.style.transition = 'all 0.5s ease';
   cart.style.transform = `rotateY(0deg) rotateX(0deg)`;
-  document.getElementById('flagID').style.transform = 'translateZ(0px)';
-  document.getElementById('flagID').style.transform = 'scale(1)';
+  document.getElementById(`BigImgID${numOfCart}`).style.transform = 'translateZ(0px)';
+  document.getElementById(`BigImgID${numOfCart}`).style.transform = 'scale(1)';
 }
 
-
-mouseLog(event: MouseEvent){
-  console.log(event);
-}
 
 
 }
