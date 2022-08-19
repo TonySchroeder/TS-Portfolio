27185468059c1b2b045c9661394914c0e64fabbc
Tony Schroeder <tony.schroeder@gmx.de>
2022-08-19 21:49:47 +0200

    add last css

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,27 +5,13 @@ import AOS from 'aos';
   selector: 'app-about-me',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
-  host: {
-    '(document:mousemove)': 'onMouseMove($event)'
-  }
-})
+ })
 export class AboutComponent implements OnInit {
-
-  mouseX: any;
-  mouseY: any;
-
-cursor:any = document.querySelector('cursor');
 
   constructor() { }
 
   ngOnInit(): void {
     AOS.init();
   }
-
-
-  onMouseMove(e: any) {
-    this.mouseX = e.pageX;
-    this.mouseY = e.pageY;
-   }
 
 }
